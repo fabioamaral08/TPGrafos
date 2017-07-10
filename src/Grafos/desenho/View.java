3d12242194af08fa56782d68cc45348904191340
Foltran <Foltran@186.217.107.50>
2017-07-10 20:54:16 -0300

    same same but different!

--- src/Grafos/desenho/View.java
+++ src/Grafos/desenho/View.java
@@ -16,6 +16,7 @@ import Grafos.Grafo;
 import Grafos.ListaAdjacencia;
 import Grafos.desenho.color.GrayScale;
 import Grafos.desenho.color.RainbowScale;
+import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
@@ -30,6 +31,7 @@ import java.util.StringTokenizer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
 import javax.swing.JFileChooser;
 import javax.swing.JPanel;
 
@@ -60,6 +62,10 @@ public class View extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane(this.view);
         thePanel = new javax.swing.JPanel();
         panelVazio = new javax.swing.JPanel();
+        panelBL = new javax.swing.JPanel();
+        buttonBusca = new javax.swing.JButton();
+        textFieldBusca = new javax.swing.JTextField();
+        labelBusca = new javax.swing.JLabel();
         jMenuBar1 = new javax.swing.JMenuBar();
         opcoes_Menu = new javax.swing.JMenu();
         carregarGrafo_Menu = new javax.swing.JMenuItem();
@@ -73,6 +79,7 @@ public class View extends javax.swing.JFrame {
         buttonT = new javax.swing.JMenuItem();
         buttonOT = new javax.swing.JMenuItem();
         buttonBL = new javax.swing.JMenuItem();
+        implement_Menu = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -86,10 +93,43 @@ public class View extends javax.swing.JFrame {
         );
         panelVazioLayout.setVerticalGroup(
             panelVazioLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 100, Short.MAX_VALUE)
+            .addGap(0, 123, Short.MAX_VALUE)
         );
 
-        thePanel.add(panelVazio, "card2");
+        thePanel.add(panelVazio, "cardVazio");
+        panelVazio.getAccessibleContext().setAccessibleName("");
+
+        panelBL.setBorder(javax.swing.BorderFactory.createTitledBorder("Busca em Largura"));
+
+        buttonBusca.setText("Buscar");
+
+        labelBusca.setText("Vértice Inicial:");
+
+        javax.swing.GroupLayout panelBLLayout = new javax.swing.GroupLayout(panelBL);
+        panelBL.setLayout(panelBLLayout);
+        panelBLLayout.setHorizontalGroup(
+            panelBLLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelBLLayout.createSequentialGroup()
+                .addGap(60, 60, 60)
+                .addComponent(labelBusca)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(textFieldBusca, javax.swing.GroupLayout.PREFERRED_SIZE, 81, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 341, Short.MAX_VALUE)
+                .addComponent(buttonBusca)
+                .addGap(60, 60, 60))
+        );
+        panelBLLayout.setVerticalGroup(
+            panelBLLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelBLLayout.createSequentialGroup()
+                .addGap(28, 28, 28)
+                .addGroup(panelBLLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(buttonBusca, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(textFieldBusca, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(labelBusca))
+                .addContainerGap(32, Short.MAX_VALUE))
+        );
+
+        thePanel.add(panelBL, "cardBusca");
 
         opcoes_Menu.setText("Opções");
 
@@ -138,6 +178,11 @@ public class View extends javax.swing.JFrame {
         algoritmos_Menu.add(buttonAGM);
 
         buttonCM.setText("Caminho Mínimo");
+        buttonCM.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                buttonCMActionPerformed(evt);
+            }
+        });
         algoritmos_Menu.add(buttonCM);
 
         buttonC.setText("Conectividade");
@@ -149,15 +194,33 @@ public class View extends javax.swing.JFrame {
         algoritmos_Menu.add(buttonC);
 
         buttonT.setText("Transposição");
+        buttonT.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                buttonTActionPerformed(evt);
+            }
+        });
         algoritmos_Menu.add(buttonT);
 
         buttonOT.setText("Ordem Topológica");
+        buttonOT.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                buttonOTActionPerformed(evt);
+            }
+        });
         algoritmos_Menu.add(buttonOT);
 
         buttonBL.setText("Busca em Largura");
+        buttonBL.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                buttonBLActionPerformed(evt);
+            }
+        });
         algoritmos_Menu.add(buttonBL);
 
         jMenuBar1.add(algoritmos_Menu);
+
+        implement_Menu.setText("Implementação");
+        jMenuBar1.add(implement_Menu);
 
         setJMenuBar(jMenuBar1);
 
@@ -165,7 +228,7 @@ public class View extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 690, Short.MAX_VALUE)
+            .addComponent(jScrollPane1)
             .addComponent(thePanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
@@ -249,7 +312,8 @@ public class View extends javax.swing.JFrame {
     }//GEN-LAST:event_salvarImagem_MenuActionPerformed
 
     private void buttonCCActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonCCActionPerformed
-        // TODO add your handling code here:
+        CardLayout card = (CardLayout)thePanel.getLayout();
+        card.show(thePanel, "cardVazio");
 
         ComponentesConexas componentesConexas = new ComponentesConexas();
         componentesConexas.execute(grafo);
@@ -266,7 +330,9 @@ public class View extends javax.swing.JFrame {
     }//GEN-LAST:event_buttonCCActionPerformed
 
     private void buttonCorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonCorActionPerformed
-        // TODO add your handling code here:
+        CardLayout card = (CardLayout)thePanel.getLayout();
+        card.show(thePanel, "cardVazio");
+        
         Coloracao coloracao = new Coloracao();
         coloracao.execute(grafo);
         int cores[] = coloracao.getCores();
@@ -285,12 +351,39 @@ public class View extends javax.swing.JFrame {
     }//GEN-LAST:event_buttonCorActionPerformed
 
     private void buttonAGMActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonAGMActionPerformed
-        // TODO add your handling code here:
+        panelBL.setBorder(BorderFactory.createTitledBorder("Árvore Geradora Mínima"));
+                
+        CardLayout card = (CardLayout)thePanel.getLayout();
+        card.show(thePanel, "cardBusca");
+        
     }//GEN-LAST:event_buttonAGMActionPerformed
 
     private void buttonCActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonCActionPerformed
-        // TODO add your handling code here:
+        CardLayout card = (CardLayout)thePanel.getLayout();
+        card.show(thePanel, "cardVazio");
     }//GEN-LAST:event_buttonCActionPerformed
+
+    private void buttonBLActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonBLActionPerformed
+        panelBL.setBorder(BorderFactory.createTitledBorder("Busca em Largura"));
+        
+        CardLayout card = (CardLayout)thePanel.getLayout();
+        card.show(thePanel, "cardBusca");
+    }//GEN-LAST:event_buttonBLActionPerformed
+
+    private void buttonOTActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonOTActionPerformed
+        CardLayout card = (CardLayout)thePanel.getLayout();
+        card.show(thePanel, "cardVazio");
+    }//GEN-LAST:event_buttonOTActionPerformed
+
+    private void buttonTActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonTActionPerformed
+        CardLayout card = (CardLayout)thePanel.getLayout();
+        card.show(thePanel, "cardVazio");
+    }//GEN-LAST:event_buttonTActionPerformed
+
+    private void buttonCMActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonCMActionPerformed
+        CardLayout card = (CardLayout)thePanel.getLayout();
+        card.show(thePanel, "cardVazio");
+    }//GEN-LAST:event_buttonCMActionPerformed
 
     public class ViewPanel extends JPanel {
 
@@ -455,6 +548,7 @@ public class View extends javax.swing.JFrame {
     private javax.swing.JMenu algoritmos_Menu;
     private javax.swing.JMenuItem buttonAGM;
     private javax.swing.JMenuItem buttonBL;
+    private javax.swing.JButton buttonBusca;
     private javax.swing.JMenuItem buttonC;
     private javax.swing.JMenuItem buttonCC;
     private javax.swing.JMenuItem buttonCM;
@@ -462,11 +556,15 @@ public class View extends javax.swing.JFrame {
     private javax.swing.JMenuItem buttonOT;
     private javax.swing.JMenuItem buttonT;
     private javax.swing.JMenuItem carregarGrafo_Menu;
+    private javax.swing.JMenu implement_Menu;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel labelBusca;
     private javax.swing.JMenu opcoes_Menu;
+    private javax.swing.JPanel panelBL;
     private javax.swing.JPanel panelVazio;
     private javax.swing.JMenuItem salvarImagem_Menu;
+    private javax.swing.JTextField textFieldBusca;
     private javax.swing.JPanel thePanel;
     // End of variables declaration//GEN-END:variables
 }
