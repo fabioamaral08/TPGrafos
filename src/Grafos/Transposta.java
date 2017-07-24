47a3f96c50469bbae3efee63fb738e3b7e000b08
Foltran <Foltran@186.217.107.125>
2017-07-24 20:21:03 -0300

    Ainda não mudei o View!!!

--- src/Grafos/Transposta.java
+++ src/Grafos/Transposta.java
@@ -11,4 +11,32 @@ package Grafos;
  */
 public class Transposta {
     
+    private Grafo Transposto;
+    
+    
+    public void execute(Grafo g){       
+        
+        Representacao Grep = g.getRepresentacao();        
+        Representacao GTrep = new ListaAdjacencia();
+        
+        int numVert = Grep.getNumVertices();
+        GTrep.init(numVert, 1);
+        No aux;
+        
+        for(int i = 0; i < numVert; i++){                                       // ENQUANTO NÃO PERCORRER TODOS OS VÉRTICES
+            aux = Grep.getAdj(i);                                               // AUX RECEBE ADJACENCIA ATUAL
+            while(aux != null){                                                 // ENQUANTO NÃO ACABAR ADJACÊNCIA
+                GTrep.addAresta(aux.getVertID(), i, aux.getPeso());             // INSERE TRANSPOSTO NA REPRESENTAÇÃO
+                aux = aux.getProx();
+            }
+        }
+       
+    }
+
+    public Grafo getTransposto() {
+        return Transposto;
+    }
+    
+    
+    
 }
